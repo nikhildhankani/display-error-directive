6ae239f9a59f9089417a1d8606484c0aeeab1c5f
nikhildhankani <nikhildhankani@gmail.com>
2016-12-15 22:46:15 -0800

    Update and rename clarity-field-error.component.ts to clr-error-template.component.ts
    Adding example for ng-error

--- clr-error-template.component.ts
+++ clr-error-template.component.ts
@@ -1,14 +1,19 @@
+/*
+ * Copyright (c) 2016 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
 import { Component,ContentChildren, ViewChild,ViewContainerRef } from '@angular/core';
 
 @Component({
   template: '<label aria-haspopup="true" role="tooltip" class="tooltip tooltip-validation tooltip-sm" [class.invalid]="error && error.length>0">'+
-                      '<template #t></template>'+
+                      '<template #t1></template>'+
                       '<span [hidden]="!(error && error.length>0)" class="tooltip-content">'+
                          '{{error}}'
                       +'</span>'+
                  '</label>'
 })
-export class ClarityFieldError {
+export class ClrErrorComponent {
   error:string='';
-  @ViewChild("t", { read: ViewContainerRef }) templateToBeReplaced;
+  @ViewChild("t1", { read: ViewContainerRef }) templateToBeReplaced;
 }
